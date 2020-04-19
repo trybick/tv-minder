42984bc2aac9fb2f2b879f10dcb115674afb86d2
Tim Rybicki <t.rybicki@gmail.com>
2020-04-19 19:47:27 -0400

    Add redesigned header

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,20 +1,59 @@
-import React from 'react';
-import { Box, Button, Flex, Text } from '@chakra-ui/core';
+import React, { ReactChildren } from 'react';
+import { Box, Button, Flex, Heading, Text } from '@chakra-ui/core';
 
-const Header = (): JSX.Element => (
-  <Flex justify="space-between" bg="grey" color="white">
-    <Box p={4}>
-      <Text fontSize="lg">TV Minder</Text>
-    </Box>
-    <Flex align="center">
-      <Button size="sm" variantColor="teal">
-        Login
-      </Button>
-      <Button size="sm" variantColor="teal" mx={4}>
-        Register
-      </Button>
-    </Flex>
-  </Flex>
+const MenuItem = ({ text }: { text: string }) => (
+  <Text mt={{ base: 4, md: 0 }} mr={6} display="block">
+    {text}
+  </Text>
 );
+
+const Header = () => {
+  const [show, setShow] = React.useState(false);
+  const handleToggle = () => setShow(!show);
+
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1.5rem"
+      bg="teal.500"
+      color="white"
+    >
+      <Flex align="center" mr={5}>
+        <Heading as="h1" size="lg" letterSpacing={'-.1rem'}>
+          TV Minder
+        </Heading>
+      </Flex>
+
+      <Box display={{ sm: 'block', md: 'none' }} onClick={handleToggle}>
+        <svg fill="white" width="12px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <title>Menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+      </Box>
+
+      <Box
+        display={{ xs: 'none', sm: show ? 'block' : 'none', md: 'flex' }}
+        width={{ sm: 'full', md: 'auto' }}
+        alignItems="center"
+        flexGrow={1}
+      >
+        <MenuItem text="Search" />
+        <MenuItem text="Calendar" />
+      </Box>
+
+      <Box
+        display={{ xs: 'none', sm: show ? 'block' : 'none', md: 'block' }}
+        mt={{ base: 4, md: 0 }}
+      >
+        <Button bg="transparent" border="1px">
+          Create account
+        </Button>
+      </Box>
+    </Flex>
+  );
+};
 
 export default Header;
