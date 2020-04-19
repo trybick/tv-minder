d0d3054cae05439df5c05b5e0af84a09e5666ad1
Tim Rybicki <t.rybicki@gmail.com>
2020-04-19 12:48:29 -0400

    Add Tag for Follow button on search result

--- front/src/components/SearchResult.tsx
+++ front/src/components/SearchResult.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Box, Heading } from '@chakra-ui/core';
+import { Box, Flex, Heading, Tag, TagIcon, TagLabel } from '@chakra-ui/core';
 
 const SearchResult = ({ show }: { show: any }) => (
-  <Box p={3} mb={4} shadow="md" borderWidth="1px" >
-    <Heading size="sm">{show.name}</Heading>
+  <Box p={3} mb={4} shadow="md" borderWidth="1px">
+    <Flex justify="space-between">
+      <Heading size="sm">{show.name}</Heading>
+      <Tag size="md" variantColor="teal">
+        <TagIcon icon="add" size="12px" />
+        <TagLabel>Follow</TagLabel>
+      </Tag>
+    </Flex>
   </Box>
 );
 
