0afadeb0a8d095c0dda9c9ed8e6b93da318567e8
Tim Rybicki <t.rybicki@gmail.com>
2020-04-19 20:19:52 -0400

    Change tag to button on Follow button

--- front/src/components/SearchResult.tsx
+++ front/src/components/SearchResult.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Badge, Box, Flex, Heading, Tag, TagIcon, TagLabel, Text } from '@chakra-ui/core';
+import { Badge, Box, Button, Flex, Heading, Text } from '@chakra-ui/core';
 
 const SearchResult = ({ show }: { show: any }) => {
   const { first_air_date: firstAirDate, name, vote_average: voteAverage } = show;
@@ -8,7 +8,7 @@ const SearchResult = ({ show }: { show: any }) => {
   return (
     <Box p={3} mb={4} shadow="md" borderWidth="1px">
       <Flex justify="space-between">
-        <Flex>
+        <Flex align="center">
           <Heading size="sm" isTruncated>
             {name}
           </Heading>
@@ -18,10 +18,9 @@ const SearchResult = ({ show }: { show: any }) => {
             </Flex>
           )}
         </Flex>
-        <Tag size="sm" variantColor="teal">
-          <TagIcon icon="add" size="12px" />
-          <TagLabel>Follow</TagLabel>
-        </Tag>
+        <Button size="sm" leftIcon="small-add" variantColor="teal" variant="outline">
+          Follow
+        </Button>
       </Flex>
       <Text fontSize="12px">{year}</Text>
     </Box>
