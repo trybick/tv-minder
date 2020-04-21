62ca1b9e36572240229d6674f62e7d772056e83d
Tim Rybicki <t.rybicki@gmail.com>
2020-04-20 20:28:56 -0400

    Add percent watching now badge

--- front/src/components/search/SearchResult.tsx
+++ front/src/components/search/SearchResult.tsx
@@ -2,27 +2,32 @@ import React from 'react';
 import { Badge, Box, Button, Flex, Heading, Text } from '@chakra-ui/core';
 
 const SearchResult = ({ show }: { show: any }) => {
-  const { first_air_date: firstAirDate, name, vote_average: voteAverage } = show;
-  const year = firstAirDate?.substr(0, 4);
+  const { first_air_date: firstAirDate, name, popularity } = show;
+  const yearForDisplay = firstAirDate?.substr(0, 4);
+  const popularityForDisplay = String(popularity)?.substr(0, 2).replace(/\.$/, '');
 
   return (
     <Box p={3} mb={4} shadow="md" borderWidth="1px">
       <Flex justify="space-between">
         <Flex align="center">
-          <Heading size="sm" isTruncated>
+          <Heading size="md" isTruncated>
             {name}
           </Heading>
-          {!!voteAverage && (
-            <Flex ml="6px" align="center">
-              <Badge>{voteAverage}</Badge>
-            </Flex>
-          )}
         </Flex>
         <Button size="sm" leftIcon="small-add" variantColor="teal" variant="outline">
           Follow
         </Button>
       </Flex>
-      <Text fontSize="12px">{year}</Text>
+      <Flex mt="4px">
+        <Badge variant="subtle">{yearForDisplay}</Badge>
+        {!!popularity && (
+          <Flex ml="6px" align="center">
+            <Badge variant="subtle" color="green.400">
+              {popularityForDisplay}% watching now
+            </Badge>
+          </Flex>
+        )}
+      </Flex>
     </Box>
   );
 };
