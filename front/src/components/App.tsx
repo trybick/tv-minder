da338d0e9129ffc5c2d120a12697e4ee6614274e
Tim Rybicki <t.rybicki@gmail.com>
2020-04-24 20:19:23 -0400

    Create folder for header components

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Box } from '@chakra-ui/core';
-import Header from 'components/Header';
+import Header from 'components/header/Header';
 import SearchContainer from 'components/search/SearchContainer';
 
 const App = (): JSX.Element => (
