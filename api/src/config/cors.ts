52aa2cfdbd0b6b8976fb024680710d3c274acb2b
Tim Rybicki <t.rybicki@gmail.com>
2020-04-25 17:27:02 -0400

    Configure cors in api

---
+++ api/src/config/cors.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from 'express';
+
+export const configureCors = (req: Request, res: Response, next: NextFunction) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+};
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,7 @@
 import 'module-alias/register';
 import express from 'express';
 import connectToDatabase from 'config/database';
+import { configureCors } from './config/cors';
 import { albumRoutes } from 'entities/routes/album';
 import { userRoutes } from 'entities/routes/user';
 
@@ -8,7 +9,9 @@ const app = express();
 const port = 5000;
 
 connectToDatabase();
+
 app.use(express.json());
+app.use(configureCors);
 
 app.use('/albums', albumRoutes);
 app.use(userRoutes);
