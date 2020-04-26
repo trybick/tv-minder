af94757f1ae0971768ac6469be80c9070a7b153c
Tim Rybicki <t.rybicki@gmail.com>
2020-04-26 10:29:10 -0400

    Create function to log details on follow click

--- front/src/components/search/SearchResult.tsx
+++ front/src/components/search/SearchResult.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { Badge, Box, Button, Flex, Heading, Text } from '@chakra-ui/core';
 
 const SearchResult = ({ show }: { show: any }) => {
-  const { first_air_date: firstAirDate, name, popularity } = show;
+  const { first_air_date: firstAirDate, id: externalId, name, popularity } = show;
   const yearForDisplay = firstAirDate?.substr(0, 4);
   const popularityForDisplay =
     popularity >= 10 && String(popularity)?.substr(0, 2).replace(/\.$/, '');
+
+  function onFollowShow() {
+    console.log('id', externalId, 'name', name);
+  }
 
   return (
     <Box p={3} mb={4} shadow="md" borderWidth="1px">
@@ -15,7 +19,13 @@ const SearchResult = ({ show }: { show: any }) => {
             {name}
           </Heading>
         </Flex>
-        <Button size="sm" leftIcon="small-add" variantColor="teal" variant="outline">
+        <Button
+          size="sm"
+          leftIcon="small-add"
+          variantColor="teal"
+          variant="outline"
+          onClick={onFollowShow}
+        >
           Follow
         </Button>
       </Flex>
