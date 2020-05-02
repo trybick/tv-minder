554e65551e2c03d6a2b2c8086b9d8153fa174936
Tim Rybicki <t.rybicki@gmail.com>
2020-05-02 16:30:34 -0400

    Make searchShows one file with request creator

--- front/src/gateway/searchShows.ts
+++ front/src/gateway/searchShows.ts
@@ -1,8 +1,16 @@
+import axios, { CancelTokenSource } from 'axios';
 import { MOVIE_DB_URL } from 'utils/constants';
-import { makeCachedRequest } from 'gateway/common/makeCachedRequest';
+
+//
+// Our app calls searchShows
+// It uses a request creator for results caching and cancel tokens
+//
+
+type RequestConfig = { api_key: string | undefined; query: string };
+const cachedResults: any = {};
 
 export const searchShows = async (query: string) => {
-  const requestConfig = {
+  const requestConfig: RequestConfig = {
     api_key: process.env.REACT_APP_API_KEY,
     query,
   };
@@ -12,3 +20,37 @@ export const searchShows = async (query: string) => {
 
   return results ? { results, totalResults } : { results: null };
 };
+
+const makeCachedRequestCreator = () => {
+  let cancelToken: CancelTokenSource;
+
+  return (url: string, requestConfig: RequestConfig) => {
+    if (cancelToken) {
+      cancelToken.cancel();
+    }
+    cancelToken = axios.CancelToken.source();
+
+    return cachedResults[requestConfig.query]
+      ? cachedResults[requestConfig.query]
+      : axios
+          .get(url, {
+            cancelToken: cancelToken.token,
+            params: requestConfig,
+          })
+          .then((res) => {
+            const { data } = res;
+            cachedResults[requestConfig.query] = data;
+
+            return data;
+          })
+          .catch((err: Error) => {
+            if (axios.isCancel(err)) {
+              console.log('Successfully canceled request');
+            } else {
+              console.log('General Axios error', err.message);
+            }
+          });
+  };
+};
+
+const makeCachedRequest = makeCachedRequestCreator();
