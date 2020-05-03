7beda440e9d96ec9f86f570010c453c0bbac9ccd
Tim Rybicki <t.rybicki@gmail.com>
2020-05-03 16:16:46 -0400

    Trim down active handleerrors messages

--- front/src/utils/handleErrors.ts
+++ front/src/utils/handleErrors.ts
@@ -8,19 +8,19 @@ export default function handleErrors(error: AxiosError) {
      * The request was made and the server responded with a
      * status code that falls out of the range of 2xx
      */
-    console.log('error status', error.response.status);
+    // console.log('error status:', error.response.status);
     console.log('error data:', error.response.data);
-    console.log('error headers:', error.response.headers);
+    // console.log('error headers:', error.response.headers);
   } else if (error.request) {
     /*
      * The request was made but no response was received, `error.request`
      * is an instance of XMLHttpRequest in the browser and an instance
      * of http.ClientRequest in Node.js
      */
-    console.log(error.request);
+    console.log('error request:', error.request);
   } else {
     // Something happened in setting up the request and triggered an Error
-    console.log('Error', error.message);
+    console.log('General error:', error.message);
   }
-  console.log('error config', error.config);
+  // console.log('error config', error.config);
 }
