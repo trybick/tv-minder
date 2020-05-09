2ebfdb6239affc76e4b6cb1a7e6c83bd1e6d1df8
Tim Rybicki <t.rybicki@gmail.com>
2020-05-09 08:42:18 -0400

    Change action functions into arrow functions

--- front/src/store/user/actions.ts
+++ front/src/store/user/actions.ts
@@ -7,44 +7,33 @@ export const SET_IS_LOGGED_IN_FALSE = 'SET_IS_LOGGED_IN_FALSE';
 export const SET_HAS_LOCAL_WARNING_TOAST_BEEN_SHOWN = 'SET_HAS_LOCAL_WARNING_TOAST_BEEN_SHOWN';
 export const FETCH_USER_FOLLOWS = 'FETCH_USER_FOLLOWS';
 
-export function setIsLoggedInAction() {
-  return (dispatch: any) => {
-    dispatch({
-      type: SET_IS_LOGGED_IN_TRUE,
-    });
-  };
-}
+export const setIsLoggedInAction = () => (dispatch: any) => {
+  dispatch({
+    type: SET_IS_LOGGED_IN_TRUE,
+  });
+};
 
-export function setIsLoggedOutAction() {
-  return (dispatch: any) => {
-    dispatch({
-      type: SET_IS_LOGGED_IN_FALSE,
-    });
-  };
-}
+export const setIsLoggedOutAction = () => (dispatch: any) => {
+  dispatch({
+    type: SET_IS_LOGGED_IN_FALSE,
+  });
+};
 
-export function setHasLocalWarningToastBeenShownAction() {
-  return (dispatch: any) => {
-    dispatch({
-      type: SET_HAS_LOCAL_WARNING_TOAST_BEEN_SHOWN,
-    });
-  };
-}
+export const setHasLocalWarningToastBeenShownAction = () => (dispatch: any) => {
+  dispatch({
+    type: SET_HAS_LOCAL_WARNING_TOAST_BEEN_SHOWN,
+  });
+};
 
-export function fetchUserFollowsAction() {
-  return (dispatch: any) => {
-    const token = localStorage.getItem('jwt');
-
-    return axios
-      .get(`${baseUrl}/follow`, {
-        params: { token },
-      })
-      .then(({ data }) => {
-        dispatch({
-          type: FETCH_USER_FOLLOWS,
-          payload: data,
-        });
-      })
-      .catch(handleErrors);
-  };
-}
+export const fetchUserFollowsAction = () => (dispatch: any) =>
+  axios
+    .get(`${baseUrl}/follow`, {
+      params: { token: localStorage.getItem('jwt') },
+    })
+    .then(({ data }) => {
+      dispatch({
+        type: FETCH_USER_FOLLOWS,
+        payload: data,
+      });
+    })
+    .catch(handleErrors);
