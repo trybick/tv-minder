a74c38bbd7a3b2715ff6831184052aa1a3548bef
Tim Rybicki <t.rybicki@gmail.com>
2020-05-13 20:16:09 -0400

    Get season episodes for array of active seasons

--- front/src/gateway/getEpisodes.ts
+++ front/src/gateway/getEpisodes.ts
@@ -6,15 +6,12 @@ type QueryParams = {
 };
 
 //
-// **Deprecated** but leaving for now
-// Now using a different API which should make getting episode data simpler
+// Using The Movie DB (first API)
 //
 
 export const getSchedule = async (showId: number) => {
   const seasonsToFetch = await getActiveSeasons(showId);
-  console.log('seasonsToFetch:', seasonsToFetch);
 
-  // NEXT call getSeasonEpisodes here with the active seasons
   const seasonEpisodes = await getSeasonEpisodes(showId, seasonsToFetch);
   console.log('seasonEpisodes:', seasonEpisodes);
 };
@@ -37,7 +34,12 @@ export const getActiveSeasons = async (showId: number): Promise<number[]> => {
         const nextSeasonNumberToAir = data.next_episode_to_air?.season_number || null;
         const activeSeasons = [lastSeasonNumberToAir, nextSeasonNumberToAir];
 
-        return activeSeasons;
+        // If last and next are the same season number, only return a single value
+        return lastSeasonNumberToAir &&
+          nextSeasonNumberToAir &&
+          lastSeasonNumberToAir === nextSeasonNumberToAir
+          ? [lastSeasonNumberToAir]
+          : activeSeasons;
       })
       .catch((err: Error) => {
         console.log('Axios error', err.message);
@@ -45,28 +47,17 @@ export const getActiveSeasons = async (showId: number): Promise<number[]> => {
   );
 };
 
-// Get full episode details for one season
+// Get full episode details for seasons
 export const getSeasonEpisodes = async (showId: number, seasonNumbers: number[]) => {
   const queryParams: QueryParams = {
     api_key: process.env.REACT_APP_API_KEY,
   };
 
-  return await seasonNumbers
-    .filter((seasonNum) => !!seasonNum)
-    .map(async (seasonNum) => {
-      const url = `${MOVIE_DB_BASE_URL}/tv/${showId}/season/${seasonNum}`;
+  const seasonRequests = seasonNumbers
+    .filter(Boolean)
+    .map((seasonNum) =>
+      axios.get(`${MOVIE_DB_BASE_URL}/tv/${showId}/season/${seasonNum}`, { params: queryParams })
+    );
 
-      return axios
-        .get(url, {
-          params: queryParams,
-        })
-        .then((res) => {
-          const { data } = res;
-
-          return data;
-        })
-        .catch((err: Error) => {
-          console.log('Axios error', err.message);
-        });
-    });
+  return axios.all(seasonRequests).then((res) => res.map((res) => res.data));
 };
---
+++ front/src/gateway/getSchedule.ts
@@ -0,0 +1,2 @@
+import axios from 'axios';
+import { MOVIE_DB_BASE_URL } from 'utils/constants';
