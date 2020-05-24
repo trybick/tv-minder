6a30897f1f17ba61578c0bea4df061bea38cd03f
Tim Rybicki <t.rybicki@gmail.com>
2020-05-23 20:01:51 -0400

    Change front end api url to https

--- front/src/utils/constants.ts
+++ front/src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const API_URLS = {
   MOVIE_DB: 'https://api.themoviedb.org/3',
   TV_MINDER:
-    process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'http://api.tv-minder.com',
+    process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://api.tv-minder.com',
 };
 
 export const emailRegex = /^([^.@]+)(\.[^.@]+)*@([^.@]+\.)+([^.@]+)$/;
