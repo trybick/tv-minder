6bdc92331ea8626b48e9f1e58e1ebb691f97a54d
Tim Rybicki <t.rybicki@gmail.com>
2020-05-28 19:39:14 -0400

    Add cors header for max age

--- api/src/config/cors.ts
+++ api/src/config/cors.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 
 export const configureCors = (req: Request, res: Response, next: NextFunction) => {
-  // Replace '*' with domain name once deployed
+  // Consider replacing '*' with domain name once deployed
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Max-Age', '86400');
+
   next();
 };
