46ccbe2f25f9a7f7c8c0153ae4201a54004ed6e6
Tim Rybicki <t.rybicki@gmail.com>
2020-05-29 11:14:58 -0400

    Add catch block to mongoose connect function

--- api/src/config/database.ts
+++ api/src/config/database.ts
@@ -4,12 +4,15 @@ import envConfig from 'config/env';
 const DATABASE_URL = `mongodb+srv://tim:${envConfig.DB_PASSWORD}@tv-minder-cluster-rrvuj.mongodb.net/main?retryWrites=true&w=majority`;
 
 export default function connectToDatabase() {
-  mongoose.connect(DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  });
+  mongoose
+    .connect(DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .catch((err: Error) => console.log('mongoose error', err));
+
   const db = mongoose.connection;
   db.on('error', (error) => console.error(error));
   db.once('open', () => console.log('Connected to database'));
