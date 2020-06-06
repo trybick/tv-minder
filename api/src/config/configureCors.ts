5ba94acc156793b1e4cc6afa73e8ab7bf462870d
Tim Rybicki <t.rybicki@gmail.com>
2020-06-06 18:21:54 -0400

    fix(api): Fix prod site origin by adding www

--- api/src/config/configureCors.ts
+++ api/src/config/configureCors.ts
@@ -1,17 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 
-const allowedOrigins = ['http://localhost:4000', 'https://tv-minder.com'];
+const allowedOrigins = ['http://localhost:4000', 'https://www.tv-minder.com'];
 
 const configureCors = (req: Request, res: Response, next: NextFunction) => {
   const { origin } = req.headers;
 
   if (origin && allowedOrigins.includes(origin as string)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.set('Access-Control-Allow-Origin', origin);
   }
 
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Max-Age', '86400');
+  res.set({
+    'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'Access-Control-Max-Age': '86400',
+  });
 
   next();
 };
