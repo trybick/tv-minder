ebd0751e78d979a0a54a91fd1944952519c63d35
Tim Rybicki <t.rybicki@gmail.com>
2020-06-06 16:19:25 -0400

    chore(api): Configure mulitple origins for CORS

--- api/src/config/cors.ts
+++ api/src/config/cors.ts
@@ -1,8 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 
+const allowedOrigins = ['http://localhost:4000', 'https://tv-minder.com'];
+
 export const configureCors = (req: Request, res: Response, next: NextFunction) => {
-  // Consider replacing '*' with domain name once deployed
-  res.header('Access-Control-Allow-Origin', '*');
+  const { origin } = req.headers;
+
+  if (origin && allowedOrigins.includes(origin as string)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Max-Age', '86400');
