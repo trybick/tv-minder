be46f7e9bdde176f969298997fe94d13c0713481
Tim Rybicki <t.rybicki@gmail.com>
2020-06-06 16:27:53 -0400

    chore(api): Replace production ENV with port ENV

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,14 +1,14 @@
 import 'module-alias/register';
 import express from 'express';
 import connectToDatabase from 'config/database';
-import { configureCors } from './config/cors';
+import { configureCors } from 'config/cors';
 import { userRoutes } from 'entities/routes/user';
 import { followShowRoutes } from 'entities/routes/followShow';
 import { healthCheckRoutes } from 'entities/routes/healthCheck';
 
-const port = process.env.NODE_ENV === 'production' ? 80 : 5000;
-
+const port = process.env.PORT || 5000;
 const app = express();
+
 app.use(configureCors);
 app.use(express.json());
 
