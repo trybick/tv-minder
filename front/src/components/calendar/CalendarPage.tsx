edb170c1dff2db3452eb669a9cb194c5d5b83646
Tim Rybicki <t.rybicki@gmail.com>
2020-06-14 14:20:19 -0400

    Check timestamp

--- front/src/components/calendar/CalendarPage.tsx
+++ front/src/components/calendar/CalendarPage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import moment from 'moment';
 import { Box } from '@chakra-ui/core';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
@@ -18,19 +19,27 @@ const CalendarPage = (): JSX.Element => {
 
   useEffect(() => {
     async function loadEpisodesForCalendar() {
-      const storedShowsIds = Object.keys(storedEpisodeData);
-      const cachedIds = followedShowsIds.filter(id => storedShowsIds.includes(String(id)));
-      const nonCachedIds = followedShowsIds.filter(id => !cachedIds.includes(id));
-
-      const cachedData = cachedIds.flatMap(id =>
+      const CACHE_DURATION = 5;
+      const cachedIds = Object.keys(storedEpisodeData);
+      const validCachedIds = followedShowsIds.filter(
+        id =>
+          cachedIds.includes(String(id)) &&
+          CACHE_DURATION > moment().diff(moment(storedEpisodeData[id].fetchedAt), 'days')
+      );
+      const cachedData = validCachedIds.flatMap(id =>
         storedEpisodeData[id].episodes !== null ? Object.values(storedEpisodeData[id].episodes) : []
       );
 
-      const { cache, fetchedEpisodeData } = await fetchEpisodeData(nonCachedIds);
-      const combinedEpisodesForDisplay = cachedData.concat(fetchedEpisodeData);
+      let fetchedData;
+      const nonCachedIds = followedShowsIds.filter(id => !validCachedIds.includes(id));
+      if (nonCachedIds) {
+        const { cache, fetchedEpisodeData } = await fetchEpisodeData(nonCachedIds);
+        fetchedData = fetchedEpisodeData;
+        dispatch(saveEpisodeDataAction(cache));
+      }
 
+      const combinedEpisodesForDisplay = cachedData.concat(fetchedData);
       setCalendarEpisodes(combinedEpisodesForDisplay);
-      dispatch(saveEpisodeDataAction(cache));
     }
 
     loadEpisodesForCalendar();
