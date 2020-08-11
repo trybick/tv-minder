98b75def2f3fd7466dd9053871d9c1b76b15ffaf
Tim Rybicki <t.rybicki@gmail.com>
2020-08-11 17:31:31 -0400

    Add more content to shows in All Shows section

--- front/src/components/myShows/AllShows.tsx
+++ front/src/components/myShows/AllShows.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Box, Grid, Heading, Image, Tooltip } from '@chakra-ui/core';
+import { Box, Grid, Heading, Image, Text, Tooltip } from '@chakra-ui/core';
 import { selectBasicShowInfoForAllShows } from 'store/tv/selectors';
 import { fallBackImage } from 'utils/constants';
 
@@ -14,9 +14,9 @@ const AllShows = () => {
         All Shows
       </Heading>
 
-      <Grid justifyContent="center" templateColumns="repeat(auto-fill, 300px)" gap={6}>
+      <Grid justifyContent="center" templateColumns="repeat(auto-fill, 400px)" gap={6}>
         {shows?.map(show => {
-          const { name, posterPath } = show;
+          const { name, numEpisodes, numSeasons, posterPath, status } = show;
           const posterSource = posterPath && `https://image.tmdb.org/t/p/w185${posterPath}`;
 
           return (
@@ -26,7 +26,7 @@ const AllShows = () => {
               key={show.id}
               p={4}
               shadow="md"
-              templateColumns="1fr 2fr"
+              templateColumns="1fr 3fr"
             >
               <Tooltip aria-label={name} label={name} placement="top" hasArrow>
                 <Box>
@@ -38,6 +38,10 @@ const AllShows = () => {
                 <Heading as="h4" fontSize="md" mb="10px" textAlign="center" isTruncated>
                   {name}
                 </Heading>
+
+                <Text fontSize="sm">{numSeasons} seasons</Text>
+                <Text fontSize="sm">{numEpisodes} episodes</Text>
+                <Text fontSize="sm">{status}</Text>
               </Box>
             </Grid>
           );
