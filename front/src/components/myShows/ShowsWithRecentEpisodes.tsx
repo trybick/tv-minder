4a697a0d591be73a066d80972811bfdd03cb1f8b
Tim Rybicki <t.rybicki@gmail.com>
2020-08-15 17:53:39 -0400

    Fix allowMulitple prop not working by converting component to function

--- front/src/components/myShows/ShowsWithRecentEpisodes.tsx
+++ front/src/components/myShows/ShowsWithRecentEpisodes.tsx
@@ -19,88 +19,90 @@ import { selectBasicShowInfoForRecentEpisodes } from 'store/tv/selectors';
 import { fallbackImage } from 'utils/constants';
 import { maybePluralize } from 'utils/formatting';
 
-const RecentEpisode = ({ show }: { show: any }) => {
-  const {
-    lastEpisodeForDisplay: { airDate, episodeNumber, name, overview, seasonNumber },
-    name: showName,
-    posterPath,
-  } = show;
-  const seasonEpisodeNumber = `S${seasonNumber} E${episodeNumber}`;
-  const posterSource = posterPath && `https://image.tmdb.org/t/p/w185${posterPath}`;
+// We had an Episode component for each accordion item, however it did not work with 'allowMulitple'
+const createAccordionItems = (shows: any) =>
+  shows.map((show: any) => {
+    const {
+      lastEpisodeForDisplay: { airDate, episodeNumber, name, overview, seasonNumber },
+      name: showName,
+      posterPath,
+    } = show;
+    const seasonEpisodeNumber = `S${seasonNumber} E${episodeNumber}`;
+    const posterSource = posterPath && `https://image.tmdb.org/t/p/w185${posterPath}`;
 
-  // Moment's 'timeFromNow' would be nice here, but recent shows display as 'X hours ago'
-  const getTimeFromNow = () => {
-    let timeFromNow;
-    const daysDiff = moment(moment().startOf('day')).diff(airDate, 'days');
-    const weeksDiff = moment.duration(daysDiff, 'days').weeks();
-    const monthsDiff = moment.duration(daysDiff, 'days').months();
+    // Moment's 'timeFromNow' would be nice here, but recent shows display as 'X hours ago'
+    const getTimeFromNow = () => {
+      let timeFromNow;
+      const daysDiff = moment(moment().startOf('day')).diff(airDate, 'days');
+      const weeksDiff = moment.duration(daysDiff, 'days').weeks();
+      const monthsDiff = moment.duration(daysDiff, 'days').months();
 
-    if (daysDiff === 1) {
-      timeFromNow = 'Yesterday';
-    } else if (daysDiff < 7) {
-      timeFromNow = `${daysDiff} ${maybePluralize(daysDiff, 'day')} ago`;
-    } else if (daysDiff < 28) {
-      timeFromNow = `${weeksDiff} ${maybePluralize(weeksDiff, 'week')} ago`;
-    } else {
-      timeFromNow = `${monthsDiff} ${maybePluralize(monthsDiff, 'month')} ago`;
-    }
+      if (daysDiff === 1) {
+        timeFromNow = 'Yesterday';
+      } else if (daysDiff < 7) {
+        timeFromNow = `${daysDiff} ${maybePluralize(daysDiff, 'day')} ago`;
+      } else if (daysDiff < 28) {
+        timeFromNow = `${weeksDiff} ${maybePluralize(weeksDiff, 'week')} ago`;
+      } else {
+        timeFromNow = `${monthsDiff} ${maybePluralize(monthsDiff, 'month')} ago`;
+      }
 
-    return timeFromNow;
-  };
+      return timeFromNow;
+    };
 
-  return (
-    <AccordionItem>
-      <AccordionHeader>
-        <Grid
-          alignItems="center"
-          gap={3}
-          justifyContent="start"
-          templateColumns="110px 120px auto"
-          width="100%"
-        >
-          <Badge variant="subtle" variantColor="red">
-            {getTimeFromNow()}
-          </Badge>
+    return (
+      <AccordionItem key={show.id}>
+        <AccordionHeader>
+          <Grid
+            alignItems="center"
+            gap={3}
+            justifyContent="start"
+            templateColumns="110px 120px auto"
+            width="100%"
+          >
+            <Badge variant="subtle" variantColor="red">
+              {getTimeFromNow()}
+            </Badge>
 
-          <Text fontSize="sm" fontWeight="600" isTruncated>
-            {seasonEpisodeNumber}
-          </Text>
+            <Text fontSize="sm" fontWeight="600" isTruncated>
+              {seasonEpisodeNumber}
+            </Text>
 
-          <Text fontSize="sm" fontWeight="600" mr="12px" isTruncated>
-            {showName}
-          </Text>
-        </Grid>
-        <AccordionIcon />
-      </AccordionHeader>
-
-      <AccordionPanel pb={4}>
-        <Grid alignItems="center" gap={6} templateColumns="100px 300px">
-          <Tooltip aria-label={showName} label={showName} placement="right" hasArrow>
-            <Box width="100px">
-              <Image borderRadius="6px" fallbackSrc={fallbackImage} src={posterSource} />
-            </Box>
-          </Tooltip>
-
-          <Grid templateRows="30px 1fr">
-            <Box>
-              <Heading as="h4" fontSize="md">
-                {name}
-              </Heading>
-            </Box>
-
-            <Box mb="8px">
-              <Text fontSize="sm">{moment(airDate).format('dddd, MMMM Do')}</Text>
-            </Box>
-
-            <Box maxH="63px" overflow="hidden">
-              <Text fontSize="sm">{overview}</Text>
-            </Box>
+            <Text fontSize="sm" fontWeight="600" mr="12px" isTruncated>
+              {showName}
+            </Text>
           </Grid>
-        </Grid>
-      </AccordionPanel>
-    </AccordionItem>
-  );
-};
+          <AccordionIcon />
+        </AccordionHeader>
+
+        <AccordionPanel pb={4}>
+          <Grid alignItems="center" gap={6} templateColumns="100px 300px">
+            <Tooltip aria-label={showName} label={showName} placement="right" hasArrow>
+              <Box width="100px">
+                <Image borderRadius="6px" fallbackSrc={fallbackImage} src={posterSource} />
+              </Box>
+            </Tooltip>
+
+            <Grid templateRows="30px 1fr">
+              <Box>
+                <Heading as="h4" fontSize="md">
+                  {name}
+                </Heading>
+              </Box>
+
+              <Box mb="8px">
+                <Text fontSize="sm">{moment(airDate).format('dddd, MMMM Do')}</Text>
+              </Box>
+
+              <Box maxH="63px" overflow="hidden">
+                <Text fontSize="sm">{overview}</Text>
+              </Box>
+            </Grid>
+          </Grid>
+        </AccordionPanel>
+      </AccordionItem>
+    );
+  });
 
 const ShowsWithRecentEpisodes = () => {
   const shows = useSelector(selectBasicShowInfoForRecentEpisodes);
@@ -116,10 +118,8 @@ const ShowsWithRecentEpisodes = () => {
         Recent
       </Heading>
 
-      <Accordion>
-        {shows?.map(show => (
-          <RecentEpisode key={show.id} show={show} />
-        ))}
+      <Accordion allowMultiple={false} allowToggle={true} defaultIndex={[-1]}>
+        {createAccordionItems(shows)}
       </Accordion>
     </Box>
   );
