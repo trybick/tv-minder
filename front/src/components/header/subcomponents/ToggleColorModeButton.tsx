b1914d8d6ea08ac6a748bdd8726afb966e056563
Luis Oliveira (xGeeks) <l.oliveira@xgeeks.io>
2020-10-02 13:59:50 +0100

    fix: alphabetize props names

--- front/src/components/header/subcomponents/ToggleColorModeButton.tsx
+++ front/src/components/header/subcomponents/ToggleColorModeButton.tsx
@@ -7,9 +7,9 @@ const ToggleColorModeButton = () => {
   return (
     <IconButton
       aria-label="toggle color mode"
+      icon={colorMode === 'light' ? 'moon' : 'sun'}
       ml=" 12px"
       onClick={toggleColorMode}
-      icon={colorMode === 'light' ? 'moon' : 'sun'}
     />
   );
 };
