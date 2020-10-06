6dc860dae13aa1907a76b73abb21d199bc398412
Alisher <31371040+alisherk@users.noreply.github.com>
2020-10-06 17:56:02 -0500

    feat(homepage): Add call to action card (#49)

--- front/src/components/search/SearchContainer.tsx
+++ front/src/components/search/SearchContainer.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
-import { Flex, Spinner, Text } from '@chakra-ui/core';
+import {
+  Flex,
+  Spinner,
+  Text,
+  Stat,
+  StatLabel,
+  StatHelpText,
+  Image,
+  StatGroup,
+  useColorMode
+} from '@chakra-ui/core';
 import { ShowSearchResult } from 'types/external';
 import SearchResults from './subcomponents/SearchResults';
+import clock from '../../images/clock.png'
 
 interface Props {
   isInputDirty: boolean;
@@ -22,11 +33,29 @@ const EmptyListMessage = () => (
   </Flex>
 );
 
-const WelcomeMessage = () => (
-  <Flex justifyContent="center">
-    <Text>Welcome, search for a show</Text>
-  </Flex>
-);
+const WelcomeMessage = () => {
+  const { colorMode } = useColorMode(); 
+  return (
+    <Flex justifyContent="center">
+      <StatGroup
+        bg={colorMode === 'light' ? 'white' : ''}
+        d="flex"
+        alignItems="center"
+        flexDirection={{ sm: 'column', lg: 'row' }}
+        borderWidth="1px"
+        padding="10px"
+        shadow="sm"
+        minHeight="120px"
+      >
+        <Stat d="flex" flexDirection="column" alignItems="center">
+          <StatLabel>Keep track of your favorite shows.</StatLabel>
+          <StatHelpText>Search for a show now!</StatHelpText>
+        </Stat>
+        <Image alt="alt clock logo" height="80px" objectFit='cover' src={clock} />
+      </StatGroup>
+    </Flex>
+  );
+};
 
 const SearchContainer = ({ isInputDirty, isLoading, shows, totalResults }: Props) => (
   <Flex justify="center" m="0 auto">
