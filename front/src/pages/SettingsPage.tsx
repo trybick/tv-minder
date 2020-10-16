7f2d6885dae71790e6a0b7754866b027a9d17ba9
SirPedr <ppagbpedro@gmail.com>
2020-10-16 17:25:57 -0300

    feat(Settings): Create 'Change Password' functionality UI

--- front/src/pages/SettingsPage.tsx
+++ front/src/pages/SettingsPage.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Box, Heading, Text } from '@chakra-ui/core';
+import { Box } from '@chakra-ui/core';
+import ChangePasswordContainer from 'components/changePassword/ChangePassword';
 
 const SettingsPage = () => (
   <Box mt="25px" textAlign="center">
-    <Heading as="h3" fontSize="1.8rem">
-      Settings
-    </Heading>
-
-    <Text mt="10px">Nothing here yet :)</Text>
+    <ChangePasswordContainer />
   </Box>
 );
 
