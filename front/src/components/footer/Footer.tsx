9785db457a6f070fd1b704b43bbc32b7918c466f
Tim Rybicki <t.rybicki@gmail.com>
2020-11-14 19:07:31 -0500

    fix(footer): Replace github icon with svg

--- front/src/components/footer/Footer.tsx
+++ front/src/components/footer/Footer.tsx
@@ -1,32 +1,33 @@
 import React from 'react';
-import { Box, Button, Divider, Flex, Link, Text, useDisclosure } from '@chakra-ui/core';
-import { FaGithub, FaRegComment } from 'react-icons/fa';
+import { Box, Button, Divider, Flex, Icon, Link, Text, useDisclosure } from '@chakra-ui/core';
+import { FaRegComment } from 'react-icons/fa';
 import FeedbackModal from './FeedbackModal';
+
+const GithubIcon = (props: any) => (
+  <svg viewBox="0 0 20 20" {...props}>
+    <path
+      d="M10 0a10 10 0 0 0-3.16 19.49c.5.1.68-.22.68-.48l-.01-1.7c-2.78.6-3.37-1.34-3.37-1.34-.46-1.16-1.11-1.47-1.11-1.47-.9-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.9 1.52 2.34 1.08 2.91.83.1-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.94 0-1.1.39-1.99 1.03-2.69a3.6 3.6 0 0 1 .1-2.64s.84-.27 2.75 1.02a9.58 9.58 0 0 1 5 0c1.91-1.3 2.75-1.02 2.75-1.02.55 1.37.2 2.4.1 2.64.64.7 1.03 1.6 1.03 2.69 0 3.84-2.34 4.68-4.57 4.93.36.31.68.92.68 1.85l-.01 2.75c0 .26.18.58.69.48A10 10 0 0 0 10 0"
+      fill="currentColor"
+    />
+  </svg>
+);
 
 const Footer = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const disclosureProps = { isOpen, onOpen, onClose };
 
   return (
-    <Box height="50px" p=".6rem">
+    <Box mb="6px">
       <Divider />
       <Flex
         alignItems="center"
         fontSize=".85rem"
         justifyContent="space-between"
-        p={{ xs: '1rem .3rem', sm: '1rem 2rem' }}
+        p={{ xs: '1.4rem', sm: '1rem 2rem' }}
       >
-        <Box fontWeight="700">
-          <Link
-            alignItems="baseline"
-            display="flex"
-            href="https://github.com/trybick/tv-minder"
-            isExternal
-          >
-            <Box as={FaGithub} display="inline" mr="4px" size="12px" verticalAlign="sub" />
-            <Text display="inline">GitHub</Text>
-          </Link>
-        </Box>
+        <Link aria-label="GitHub" href="https://github.com/trybick/tv-minder" isExternal>
+          <Icon as={GithubIcon} h="1.7rem" id="bubbles" transition="color 0.2s" w="1.7rem" />
+        </Link>
 
         <Box color="#a0a4a6" ml="auto">
           © {new Date().getFullYear()}
