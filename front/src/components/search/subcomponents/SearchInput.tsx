ed811b9149c7d6894fcc9e6f844d631735634a04
Tim Rybicki <t.rybicki@gmail.com>
2020-11-28 12:36:42 -0500

    fix(search-input): Clear input on Esc key

--- front/src/components/search/subcomponents/SearchInput.tsx
+++ front/src/components/search/subcomponents/SearchInput.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, RefObject } from 'react';
+import React, { ChangeEvent, RefObject, useEffect } from 'react';
 import {
   Flex,
   Icon,
@@ -16,43 +16,56 @@ interface Props {
   inputValue: string;
 }
 
-const SearchInput = ({ handleChange, handleClearInput, inputRef, inputValue }: Props) => (
-  <Flex
-    direction="column"
-    justify="center"
-    m="40px auto 30px"
-    p="0 25px"
-    w={['100%%', 'sm', 'md', 'lg']}
-  >
-    <InputGroup display="flex">
-      <InputLeftElement top="5px">
-        <Icon color="gray.300" name="search-2" />
-      </InputLeftElement>
-      <Input
-        border="2px solid #0099DB"
-        borderRadius="5px"
-        height="50px"
-        onChange={handleChange}
-        placeholder="Search for a tv show"
-        ref={inputRef}
-        size="sm"
-        value={inputValue}
-        variant="flushed"
-        autoFocus
-      />
-      {inputValue && (
-        <InputRightElement right="5px" top="5px">
-          <IconButton
-            aria-label="Clear input"
-            icon="small-close"
-            onClick={handleClearInput}
-            size="md"
-            variant="ghost"
-          />
-        </InputRightElement>
-      )}
-    </InputGroup>
-  </Flex>
-);
+const SearchInput = ({ handleChange, handleClearInput, inputRef, inputValue }: Props) => {
+  function clearOnEsc(event: KeyboardEvent) {
+    event.key === 'Escape' && handleClearInput();
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', clearOnEsc, false);
+    return () => {
+      document.removeEventListener('keydown', clearOnEsc, false);
+    };
+  }, []);
+
+  return (
+    <Flex
+      direction="column"
+      justify="center"
+      m="40px auto 30px"
+      p="0 25px"
+      w={['100%%', 'sm', 'md', 'lg']}
+    >
+      <InputGroup display="flex">
+        <InputLeftElement top="5px">
+          <Icon color="gray.300" name="search-2" />
+        </InputLeftElement>
+        <Input
+          border="2px solid #0099DB"
+          borderRadius="5px"
+          height="50px"
+          onChange={handleChange}
+          placeholder="Search for a tv show"
+          ref={inputRef}
+          size="sm"
+          value={inputValue}
+          variant="flushed"
+          autoFocus
+        />
+        {inputValue && (
+          <InputRightElement right="5px" top="5px">
+            <IconButton
+              aria-label="Clear input"
+              icon="small-close"
+              onClick={handleClearInput}
+              size="md"
+              variant="ghost"
+            />
+          </InputRightElement>
+        )}
+      </InputGroup>
+    </Flex>
+  );
+};
 
 export default SearchInput;
