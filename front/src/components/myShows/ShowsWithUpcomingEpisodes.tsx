c37538ed97db960a3c11a9425b69e262312589d4
Tim Rybicki <t.rybicki@gmail.com>
2022-01-17 14:48:17 -0500

    refactor(accordions): use useCallback for accordion creations

--- front/src/components/myShows/ShowsWithUpcomingEpisodes.tsx
+++ front/src/components/myShows/ShowsWithUpcomingEpisodes.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import {
   Accordion,
@@ -19,83 +19,85 @@ import Truncate from 'react-truncate';
 import { selectBasicShowInfoForUpcomingEpisodes } from 'store/tv/selectors';
 import { fallbackImagePath } from 'constants/strings';
 import { getTimeFromNowForUpcoming } from './common/utils';
-import { BasicShowInfo } from 'types/external';
 import { useIsMobile } from 'hooks/useIsMobile';
-
-const createAccordionItems = (shows: BasicShowInfo[], isMobile: boolean) =>
-  shows.map(show => {
-    const {
-      nextEpisodeForDisplay: { airDate, episodeNumber, name, overview, seasonNumber },
-      name: showName,
-      posterPath,
-    } = show;
-    const seasonEpisodeNumber = `S${seasonNumber} E${episodeNumber}`;
-    const posterSource = posterPath && `https://image.tmdb.org/t/p/w185${posterPath}`;
-
-    return (
-      <AccordionItem key={show.id}>
-        <AccordionHeader>
-          <Grid
-            alignItems="center"
-            gap={3}
-            justifyContent="start"
-            templateColumns={{ base: '110px 70px auto', xl: '110px 120px auto' }}
-            width="100%"
-          >
-            <Badge variant="subtle" variantColor="purple">
-              {getTimeFromNowForUpcoming(airDate)}
-            </Badge>
-
-            <Text fontSize="sm" fontWeight="600" isTruncated>
-              {seasonEpisodeNumber}
-            </Text>
-
-            <Text fontSize="sm" fontWeight="600" mr="12px" isTruncated>
-              {showName}
-            </Text>
-          </Grid>
-          <AccordionIcon />
-        </AccordionHeader>
-
-        <AccordionPanel pb={4}>
-          <Grid alignItems="center" gap={6} templateColumns="1fr 3fr">
-            <Tooltip aria-label={showName} label={showName} placement="right" hasArrow>
-              <Box>
-                <Image
-                  alt={`${showName} image`}
-                  borderRadius="6px"
-                  fallbackSrc={fallbackImagePath}
-                  src={posterSource}
-                />
-              </Box>
-            </Tooltip>
-
-            <Grid templateRows="24px 1fr">
-              <Heading as="h4" fontSize="md" isTruncated>
-                {name}
-              </Heading>
-
-              <Box mb="8px">
-                <Text as="i" fontSize="sm">
-                  {moment(airDate).format('dddd, MMMM Do')}
-                </Text>
-              </Box>
-
-              <Box maxH="63px" overflow="hidden">
-                <Text fontSize="sm">
-                  <Truncate lines={isMobile ? 2 : 3}>{overview}</Truncate>
-                </Text>
-              </Box>
-            </Grid>
-          </Grid>
-        </AccordionPanel>
-      </AccordionItem>
-    );
-  });
 
 const ShowsWithUpcomingEpisodes = () => {
   const upcomingShows = useSelector(selectBasicShowInfoForUpcomingEpisodes);
   const isMobile = useIsMobile();
+
+  const createAccordionItems = useCallback(
+    () =>
+      upcomingShows.map(show => {
+        const {
+          nextEpisodeForDisplay: { airDate, episodeNumber, name, overview, seasonNumber },
+          name: showName,
+          posterPath,
+        } = show;
+        const seasonEpisodeNumber = `S${seasonNumber} E${episodeNumber}`;
+        const posterSource = posterPath && `https://image.tmdb.org/t/p/w185${posterPath}`;
+
+        return (
+          <AccordionItem key={show.id}>
+            <AccordionHeader>
+              <Grid
+                alignItems="center"
+                gap={3}
+                justifyContent="start"
+                templateColumns={{ base: '110px 70px auto', xl: '110px 120px auto' }}
+                width="100%"
+              >
+                <Badge variant="subtle" variantColor="purple">
+                  {getTimeFromNowForUpcoming(airDate)}
+                </Badge>
+
+                <Text fontSize="sm" fontWeight="600" isTruncated>
+                  {seasonEpisodeNumber}
+                </Text>
+
+                <Text fontSize="sm" fontWeight="600" mr="12px" isTruncated>
+                  {showName}
+                </Text>
+              </Grid>
+              <AccordionIcon />
+            </AccordionHeader>
+
+            <AccordionPanel pb={4}>
+              <Grid alignItems="center" gap={6} templateColumns="1fr 3fr">
+                <Tooltip aria-label={showName} label={showName} placement="right" hasArrow>
+                  <Box>
+                    <Image
+                      alt={`${showName} image`}
+                      borderRadius="6px"
+                      fallbackSrc={fallbackImagePath}
+                      src={posterSource}
+                    />
+                  </Box>
+                </Tooltip>
+
+                <Grid templateRows="24px 1fr">
+                  <Heading as="h4" fontSize="md" isTruncated>
+                    {name}
+                  </Heading>
+
+                  <Box mb="8px">
+                    <Text as="i" fontSize="sm">
+                      {moment(airDate).format('dddd, MMMM Do')}
+                    </Text>
+                  </Box>
+
+                  <Box maxH="63px" overflow="hidden">
+                    <Text fontSize="sm">
+                      <Truncate lines={isMobile ? 2 : 3}>{overview}</Truncate>
+                    </Text>
+                  </Box>
+                </Grid>
+              </Grid>
+            </AccordionPanel>
+          </AccordionItem>
+        );
+      }),
+    [upcomingShows, isMobile]
+  );
 
   return (
     <Box
@@ -109,7 +111,7 @@ const ShowsWithUpcomingEpisodes = () => {
 
       {upcomingShows.length ? (
         <Accordion allowMultiple={false} allowToggle={true} defaultIndex={[-1]}>
-          {createAccordionItems(upcomingShows, isMobile)}
+          {createAccordionItems()}
         </Accordion>
       ) : (
         <Box>
