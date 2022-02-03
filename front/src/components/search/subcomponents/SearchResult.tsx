6d4898ee689da55c86678f972a456e09a157090d
Tim Rybicki <t.rybicki@gmail.com>
2022-02-03 15:03:48 -0500

    feat: add link to show page from search result

--- front/src/components/search/subcomponents/SearchResult.tsx
+++ front/src/components/search/subcomponents/SearchResult.tsx
@@ -1,4 +1,5 @@
-import { Badge, Box, Flex, Grid, Heading, Image, Text } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+import { Badge, Box, Flex, Grid, Heading, Image, Link, Text } from '@chakra-ui/react';
 import { AppThunkPlainAction } from 'store';
 import { ID } from 'types/common';
 import { ShowSearchResult } from 'types/external';
@@ -30,14 +31,18 @@ const SearchResult = ({ showToDisplay }: Props) => {
     <Box borderWidth="1px" mb={4} p={3} shadow="md">
       <Grid alignItems="center" gap={6} templateColumns="50px 1fr">
         <Box w="50px">
-          <Image borderRadius="6px" fallbackSrc={fallbackImagePath} src={posterSource} />
+          <Link as={RouterLink} to={`/show/${showId}`}>
+            <Image borderRadius="6px" fallbackSrc={fallbackImagePath} src={posterSource} />
+          </Link>
         </Box>
 
         <Box minW="0">
           <Flex justify="space-between">
-            <Heading mr="10px" size="md" isTruncated>
-              {name}
-            </Heading>
+            <Link as={RouterLink} to={`/show/${showId}`}>
+              <Heading mr="10px" size="md" isTruncated>
+                {name}
+              </Heading>
+            </Link>
             <FollowButton showId={showId} />
           </Flex>
 
