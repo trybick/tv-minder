3669bed61efd4b8cdbf0a4666b9868e562c4fb36
trybick <t.rybicki@gmail.com>
2022-05-08 11:53:14 -0400

    Only init sentry in prod

--- front/src/utils/sentry.ts
+++ front/src/utils/sentry.ts
@@ -4,9 +4,11 @@ import { BrowserTracing } from '@sentry/tracing';
 const dsn = 'https://a573ad66c244456eb251808c6d79d851@o367043.ingest.sentry.io/6386089';
 
 export const initSentry = () => {
-  Sentry.init({
-    dsn,
-    integrations: [new BrowserTracing()],
-    tracesSampleRate: 1.0,
-  });
+  if (process.env.NODE_ENV !== 'development') {
+    Sentry.init({
+      dsn,
+      integrations: [new BrowserTracing()],
+      tracesSampleRate: 1.0,
+    });
+  }
 };
