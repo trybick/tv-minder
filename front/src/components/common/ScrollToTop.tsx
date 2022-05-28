a4eec851f2251345fd985e6c6097a93245fe5662
trybick <t.rybicki@gmail.com>
2022-05-28 06:55:53 -0400

    Fix unnecessary scroll to top when going back to My Shows

--- front/src/components/common/ScrollToTop.tsx
+++ front/src/components/common/ScrollToTop.tsx
@@ -4,8 +4,12 @@ import { useLocation } from 'react-router-dom';
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
+  // When the route changes, scroll to top of page to solve issue of Show page loading scrolled down
   useEffect(() => {
-    window.scrollTo(0, 0);
+    // Allow the scroll the maintain when going back to My Shows page
+    if (pathname !== '/my-shows') {
+      window.scrollTo(0, 0);
+    }
   }, [pathname]);
 
   return null;
