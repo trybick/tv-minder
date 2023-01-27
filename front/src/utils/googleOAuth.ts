73d29cf6b710897c5587243659dbec64074639dc
trybick <t.rybicki@gmail.com>
2023-01-27 10:08:05 -0500

    Create decode helper function

--- front/src/utils/googleOAuth.ts
+++ front/src/utils/googleOAuth.ts
@@ -14,42 +14,54 @@ type Props = {
   unregisteredClearFollowedShows: AppThunkPlainAction;
 };
 
+const decodeGooglePayload = (credential: string) => {
+  const base64Payload = credential.split('.')[1];
+  const bufferPayload = Buffer.from(base64Payload, 'base64');
+  const { email, sub: googleId } = JSON.parse(bufferPayload.toString());
+  return { email, googleId };
+};
+
 export const handleGoogleLoginSuccess = (
   response: CredentialResponse,
   { onClose, setIsLoggedIn, toast, unregisteredClearFollowedShows }: Props
 ) => {
-  if ('credential' in response && typeof response.credential === 'string') {
-    const base64Payload = response.credential.split('.')[1];
-    const bufferPayload = Buffer.from(base64Payload, 'base64');
-    const { email, sub: googleId } = JSON.parse(bufferPayload.toString());
-
-    axios
-      .post(`${API.TV_MINDER}/register`, {
-        email,
-        password: googleId,
-        isGoogleLogin: true,
-      })
-      .then(() => {
-        axios
-          .post(`${API.TV_MINDER}/login`, {
-            email,
-            password: googleId,
-          })
-          .then(res => {
-            localStorage.setItem('jwt', res.data.token);
-            onClose();
-            setIsLoggedIn(res.data.email);
-            unregisteredClearFollowedShows();
-            toast({
-              title: 'Login Successful',
-              description: 'You are now logged in with Google.',
-              status: 'success',
-              isClosable: true,
-            });
-          })
-          .catch((error: any) => {
-            handleErrors(error);
-          });
-      });
+  if (!('credential' in response) || !(typeof response.credential === 'string')) {
+    throw Error('credential field missing');
   }
+  const { email, googleId } = decodeGooglePayload(response.credential);
+
+  axios
+    .post(`${API.TV_MINDER}/register`, {
+      email,
+      password: googleId,
+      isGoogleLogin: true,
+    })
+    .then(() => {
+      axios
+        .post(`${API.TV_MINDER}/login`, {
+          email,
+          password: googleId,
+        })
+        .then(res => {
+          localStorage.setItem('jwt', res.data.token);
+          onClose();
+          setIsLoggedIn(res.data.email);
+          unregisteredClearFollowedShows();
+          toast({
+            title: 'Login Successful',
+            description: 'You are now logged in with Google.',
+            status: 'success',
+            isClosable: true,
+          });
+        })
+        .catch((error: any) => {
+          handleErrors(error);
+          toast({
+            title: 'Error in login',
+            description: 'Could not login in. Please try again.',
+            status: 'error',
+            isClosable: true,
+          });
+        });
+    });
 };
