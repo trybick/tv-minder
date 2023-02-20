8e7d104d753a56d3ad8c45df7f7d8feb1de9a2ba
trybick <t.rybicki@gmail.com>
2023-02-20 10:34:01 -0500

    Add TMDB attribution in footer

--- front/src/components/footer/Footer.tsx
+++ front/src/components/footer/Footer.tsx
@@ -1,5 +1,16 @@
-import { Box, Button, Divider, Flex, Icon, Link, Text, useDisclosure } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Icon,
+  Image,
+  Link,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { FaGithub, FaRegComment } from 'react-icons/fa';
+import TMDBLogo from 'images/TMDB-logo.svg';
 import FeedbackModal from './FeedbackModal';
 
 const Footer = () => {
@@ -15,20 +26,28 @@ const Footer = () => {
         justifyContent="space-between"
         p={{ base: '1.4rem', sm: '1rem 2rem' }}
       >
-        <Link aria-label="GitHub" href="https://github.com/trybick/tv-minder" isExternal>
-          <Icon as={FaGithub} h="1.7rem" id="bubbles" transition="color 0.2s" w="1.3rem" />
-        </Link>
+        <Flex flexDirection="column" gap="3px">
+          <Text fontSize="11px" fontWeight="600">
+            Data provided by
+          </Text>
+          <Link href="https://www.themoviedb.org/" isExternal>
+            <Image alt="The Movie DB logo" h="13px" src={TMDBLogo} />
+          </Link>
+        </Flex>
 
-        <Box color="#a0a4a6" ml="auto">
-          © {new Date().getFullYear()}
-          {` `}
-          <Text display="inline">TV-Minder</Text>
-        </Box>
+        <Link
+          aria-label="GitHub"
+          display="flex"
+          href="https://github.com/trybick/tv-minder"
+          isExternal
+        >
+          <Icon as={FaGithub} h="1.7rem" transition="color 0.2s" w="1.3rem" />
+        </Link>
 
         <Button
           bg="primary"
           color="white"
-          ml="auto"
+          // ml="auto"
           onClick={onOpen}
           rightIcon={<FaRegComment />}
           size="sm"
