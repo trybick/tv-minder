9fa64eb64b447b6b38cc1e15243e5892632442e6
trybick <t.rybicki@gmail.com>
2025-05-21 10:54:06 -0400

    Base

--- api/src/entities/routes/contact.ts
+++ api/src/entities/routes/contact.ts
@@ -5,7 +5,7 @@ import envConfig from 'config/env';
 const contactRoutes = express.Router();
 
 contactRoutes.post('/contact', (req, res) => {
-  const { text } = req.body;
+  const { text, email } = req.body;
 
   const client = nodemailer.createTransport({
     service: 'SendGrid',
@@ -14,16 +14,20 @@ contactRoutes.post('/contact', (req, res) => {
       pass: envConfig.SENDGRID_KEY,
     },
   });
-  const email = {
+
+  const emailContent = email ? `User email: ${email}\n\n${text}` : text;
+
+  const emailData = {
     from: 'admin@em792.timr.dev',
     to: 'devtimr@gmail.com',
     subject: 'TV Minder: New feedback',
-    text,
+    text: emailContent,
   };
 
-  client.sendMail(email, (error, info) => {
+  client.sendMail(emailData, (error, info) => {
     if (error) {
       console.log('Nodemailer error: ', error);
+      res.status(500).json({ message: 'Failed to send message' });
     } else {
       console.log('Email sent: ' + info.response);
       res.status(200).json({ message: 'Message sent' });
