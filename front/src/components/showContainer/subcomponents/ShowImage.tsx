fc88815e6cb768e8a1f8b9dd922bef6510d8dca3
trybick <t.rybicki@gmail.com>
2025-05-30 19:47:06 -0400

    Skeleton for mobile image

--- front/src/components/showContainer/subcomponents/ShowImage.tsx
+++ front/src/components/showContainer/subcomponents/ShowImage.tsx
@@ -1,10 +1,12 @@
 import { Image } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
 
+import DelayedSkeleton from '~/components/common/DelayedSkeleton';
 import { useIsMobile } from '~/hooks/useIsMobile';
 import { useAppSelector } from '~/store';
 import {
   selectCurrentShowInfo,
+  selectIsLoadingBasicShowInfoForShow,
   selectShowDataFromHistory,
 } from '~/store/tv/selectors';
 import { createImageUrl } from '~/utils/createImageUrl';
@@ -12,23 +14,26 @@ import { getShowIdFromUrl } from '~/utils/getShowIdFromUrl';
 
 const ShowImage = () => {
   const isMobile = useIsMobile();
+  const isLoading = useAppSelector(selectIsLoadingBasicShowInfoForShow);
   const currentShowInfo = useAppSelector(selectCurrentShowInfo);
   const showDataFromHistory = useSelector(selectShowDataFromHistory);
   const showId = getShowIdFromUrl();
 
   if (isMobile) {
     return (
-      <Image
-        left="50%"
-        maxW="100vw"
-        ml="-50vw"
-        mr="-50vw"
-        position="relative"
-        right="50%"
-        src={createImageUrl(currentShowInfo?.backdropPath)}
-        width="100vw"
-        viewTransitionName={`show-image-${showId}`}
-      />
+      <DelayedSkeleton isLoading={isLoading} h="200px" w="100%">
+        <Image
+          left="50%"
+          maxW="100vw"
+          ml="-50vw"
+          mr="-50vw"
+          position="relative"
+          right="50%"
+          src={createImageUrl(currentShowInfo?.backdropPath)}
+          width="100vw"
+          viewTransitionName={`show-image-${showId}`}
+        />
+      </DelayedSkeleton>
     );
   }
 
