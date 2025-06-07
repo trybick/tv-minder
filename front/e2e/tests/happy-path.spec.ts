1bc3f5178983c6d69f7455a1e056b1d294ef3b0b
trybick <t.rybicki@gmail.com>
2025-06-07 12:10:17 -0400

    WIP first happy path test written

--- front/e2e/tests/happy-path.spec.ts
+++ front/e2e/tests/happy-path.spec.ts
@@ -1,11 +1,13 @@
 import { expect, test } from '@playwright/test';
 
-import { popularShowsResponse, searchLastOfUsResponse } from '../mockData';
+import { popularShowsResponse, searchPokerFaceResponse } from '../mockData';
 import { mockRequest } from '../mockRequest';
 import { baseUrl } from '../playwright.config';
 
 test.describe('Happy Path', () => {
   test.beforeEach(async ({ page }) => {
+    await page.clock.setFixedTime(new Date('2025-06-06T10:00:00'));
+
     mockRequest({
       page,
       route: '**/api.themoviedb.org/3/trending/tv/week',
@@ -15,35 +17,52 @@ test.describe('Happy Path', () => {
     mockRequest({
       page,
       route: '**/api.themoviedb.org/3/search/tv**&query=game+of+thrones',
-      body: searchLastOfUsResponse,
+      body: searchPokerFaceResponse,
     });
   });
 
   test('happy path main features for logged out user', async ({ page }) => {
     await page.goto(baseUrl);
 
-    await page.getByRole('button', { name: 'follow-button-232766' }).click();
+    // Follow Mobland
     await page.getByRole('button', { name: 'follow-button-247718' }).click();
-
     await expect(page.getByRole('status')).toBeVisible();
     await expect(page.getByRole('status')).toHaveText(
       /we're saving your shows/i
     );
 
-    await page.getByPlaceholder(/find tv shows/i).fill('last of us');
+    await page.getByPlaceholder(/find tv shows/i).fill('poker face');
+    await expect(page.getByLabel(/search-result/)).toHaveCount(2);
 
-    await expect(page.getByLabel('search-result-The Last of Us')).toBeVisible();
+    await page.getByRole('button', { name: 'follow-button-120998' }).click();
+    await page
+      .getByRole('link', { name: /poker face/i })
+      .first()
+      .click();
     await expect(
-      page.getByLabel('search-result-The Last of Us Development Series')
-    ).toBeVisible();
+      page.getByRole('button', { name: 'follow-button-120998' })
+    ).toHaveText(/unfollow/i);
+
+    await page.getByRole('button', { name: 'calendar' }).click();
+    await expect(page.getByRole('heading', { name: 'June' })).toBeVisible();
+
+    await expect(page.getByRole('link', { name: /mobland/i })).toHaveCount(1);
+    await expect(page.getByRole('link', { name: /poker face/i })).toHaveCount(
+      5
+    );
+
+    await page
+      .getByRole('link', { name: /poker face/i })
+      .first()
+      .click();
     await expect(
-      page.getByLabel('search-result-Autopsy: The Last Hours of...')
+      page.getByRole('heading', { name: 'Poker Face' })
     ).toBeVisible();
 
-    await page.getByRole('button', { name: 'follow-button-100088' }).click();
-
-    await page.getByRole('link', { name: 'Calendar' }).click();
-    await page.getByRole('link', { name: 'Episodes' }).click();
+    await page.getByRole('button', { name: 'follow-button-232766' }).click();
+    await expect(
+      page.getByRole('button', { name: 'follow-button-232766' })
+    ).toHaveText(/follow/i);
   });
 
   test('happy path main features for logged in user', async ({ page }) => {
