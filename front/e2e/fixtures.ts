e5959d0baf1fe474f7cb06b45b49fa977ca64598
trybick <t.rybicki@gmail.com>
2025-06-08 15:36:42 -0400

    Mock popular and top rated

--- front/e2e/fixtures.ts
+++ front/e2e/fixtures.ts
@@ -1,15 +1,22 @@
 import { test as base, expect, Page } from '@playwright/test';
 
-import { searchPokerFaceResponse } from './mockData';
+import { popularShowsResponse } from './mockData';
+import { topRatedShowsResponse } from './mockData/topRatedShows';
 import { mockRequest } from './mockRequest';
 
 export const test = base.extend<{ page: Page }>({
   page: async ({ page }, use) => {
     mockRequest({
       page,
-      path: '/api.themoviedb.org/3/search/tv**&query=poker+face',
-      body: searchPokerFaceResponse,
+      path: '/api.themoviedb.org/3/trending/tv/week**',
+      body: popularShowsResponse,
     });
+    mockRequest({
+      page,
+      path: '/api.themoviedb.org/3/tv/top_rated**',
+      body: topRatedShowsResponse,
+    });
+
     await use(page);
   },
 });
