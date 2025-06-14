67e5d003711478b297c3fee640a6c6ec73637d0e
trybick <t.rybicki@gmail.com>
2025-06-13 20:23:01 -0400

    Change reporter

--- front/playwright.config.ts
+++ front/playwright.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
   globalSetup: './e2e/config/global-setup.ts',
   fullyParallel: true,
   // forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: process.env.CI ? 'github' : 'html',
   outputDir: './e2e/test-results',
 
   projects: [
