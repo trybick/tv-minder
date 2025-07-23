bfcd7068bfcde613d7358779da3a3d394d9263fa
trybick <t.rybicki@gmail.com>
2025-07-22 20:06:10 -0400

    Add position absolute

--- front/src/features/header/HeaderMobile.tsx
+++ front/src/features/header/HeaderMobile.tsx
@@ -27,48 +27,57 @@ const HeaderMobile = () => {
 
   return (
     <>
-      <Flex
-        align="center"
-        as="nav"
-        p="15px 24px"
-        wrap="wrap"
-        ref={headerWrapperRef}
-      >
-        <Logo onClose={closeHeader} />
+      <Box position="relative" ref={headerWrapperRef}>
+        <Flex align="center" as="nav" p="15px 24px" wrap="wrap">
+          <Logo onClose={closeHeader} />
 
-        <Box cursor="pointer" onClick={toggleHeader}>
-          <svg
-            fill="teal"
-            viewBox="0 0 20 20"
-            width="24px"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </Box>
+          <Box cursor="pointer" onClick={toggleHeader}>
+            <svg
+              fill="teal"
+              viewBox="0 0 20 20"
+              width="24px"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </Box>
+        </Flex>
 
         {isHeaderOpen && (
-          <Flex pt="6px" w="full" direction="column" gap="2px">
-            <NavigationLinks onClose={closeHeader} />
+          <Box
+            position="absolute"
+            left="0"
+            right="0"
+            bg="bg"
+            borderWidth="1px"
+            borderColor="bg"
+            borderTopWidth="0"
+            shadow="lg"
+            zIndex="dropdown"
+            p="18px 24px"
+          >
+            <Flex direction="column" gap="2px">
+              <NavigationLinks onClose={closeHeader} />
 
-            <Flex alignItems="center" gap="12px" mt={4} ml="auto">
-              <ColorModeButton
-                onClick={() => applyViewTransition(toggleColorMode)}
-              />
+              <Flex alignItems="center" gap="12px" mt={4} ml="auto">
+                <ColorModeButton
+                  onClick={() => applyViewTransition(toggleColorMode)}
+                />
 
-              {isLoggedIn ? (
-                <LogoutButton />
-              ) : (
-                <>
-                  <SignUpButton />
-                  <LoginButton />
-                </>
-              )}
+                {isLoggedIn ? (
+                  <LogoutButton />
+                ) : (
+                  <>
+                    <SignUpButton />
+                    <LoginButton />
+                  </>
+                )}
+              </Flex>
             </Flex>
-          </Flex>
+          </Box>
         )}
-      </Flex>
+      </Box>
 
       {!isShowPage && <Separator size="md" />}
     </>
