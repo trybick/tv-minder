a11b5761bd01c4fc62fd76482ba807cd80ee7aad
trybick <t.rybicki@gmail.com>
2025-07-24 20:05:40 -0400

    Remove healthcheck route

--- api/src/server.ts
+++ api/src/server.ts
@@ -4,7 +4,6 @@ import connectToDatabase from 'config/database';
 import configureCors from 'config/configureCors';
 import userRoutes from 'entities/routes/user';
 import followShowRoutes from 'entities/routes/follow';
-import healthCheckRoutes from 'entities/routes/healthCheck';
 import contactRoutes from 'entities/routes/contact';
 
 const port = process.env.PORT || 5000;
@@ -17,7 +16,6 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 connectToDatabase();
 
-app.use(healthCheckRoutes);
 app.use(userRoutes);
 app.use(followShowRoutes);
 app.use(contactRoutes);
