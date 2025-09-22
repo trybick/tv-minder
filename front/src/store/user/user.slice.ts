b967442bd770e4abec3d38dd61c1b2458f78ec33
trybick <t.rybicki@gmail.com>
2025-09-22 20:31:35 +0800

    Sentry user

--- front/src/store/user/user.slice.ts
+++ front/src/store/user/user.slice.ts
@@ -25,7 +25,7 @@ const userSlice = createSlice({
       state.hasLocalWarningToastBeenShown = true;
     },
     setIsLoggedOut: state => {
-      Sentry.getCurrentScope().setUser(null);
+      Sentry.setUser(null);
       state.isLoggedIn = false;
       state.email = '';
     },
