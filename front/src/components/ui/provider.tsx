e429926c0673b4c9463c5b27756e2f1bd799c0ec
trybick <t.rybicki@gmail.com>
2026-01-01 16:32:18 -0500

    Base

--- front/src/components/ui/provider.tsx
+++ front/src/components/ui/provider.tsx
@@ -9,7 +9,7 @@ import { ColorModeProvider, type ColorModeProviderProps } from './color-mode';
 export function Provider(props: ColorModeProviderProps) {
   return (
     <ChakraProvider value={system}>
-      <ColorModeProvider {...props} />
+      <ColorModeProvider forcedTheme="dark" {...props} />
     </ChakraProvider>
   );
 }
