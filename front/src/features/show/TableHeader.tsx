9c3b599a28cf65b587017d76cbd6deec2323c40f
Tim Rybicki <39889198+trybick@users.noreply.github.com>
2026-01-03 14:36:54 -0500

    feat: improve episodes table appearance (#183)

---
+++ front/src/features/show/TableHeader.tsx
@@ -0,0 +1,19 @@
+import { Text, TextProps } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
+const TableHeader = ({
+  children,
+  ...textProps
+}: { children: ReactNode } & TextProps) => (
+  <Text
+    color="fg.muted"
+    fontSize="xs"
+    fontWeight="bold"
+    textTransform="uppercase"
+    {...textProps}
+  >
+    {children}
+  </Text>
+);
+
+export default TableHeader;
