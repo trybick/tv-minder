6b100ff5db00ef6f503bea60951190beec1efd49
trybick <t.rybicki@gmail.com>
2026-01-18 20:09:11 -0500

    Add icon

--- front/src/features/show/ShowDetails.tsx
+++ front/src/features/show/ShowDetails.tsx
@@ -12,7 +12,7 @@ import {
 } from '@chakra-ui/react';
 import { BsFillPersonFill } from 'react-icons/bs';
 import { FaStar } from 'react-icons/fa';
-import { HiOutlineVideoCamera } from 'react-icons/hi';
+import { HiOutlineCalendar, HiOutlineVideoCamera } from 'react-icons/hi';
 import { IoIosTimer } from 'react-icons/io';
 import { TbLanguage } from 'react-icons/tb';
 
@@ -195,6 +195,29 @@ export const ShowDetails = () => {
       {/* Air Dates Section */}
       {!isLoading && hasAirDates && (
         <Grid gap={4} templateColumns={{ base: '1fr', sm: '1fr 1fr' }} mb={8}>
+          {nextEpisodeAirDate && (
+            <Stat.Root
+              p={4}
+              bg="whiteAlpha.100"
+              borderRadius="xl"
+              borderWidth="1px"
+              borderColor="whiteAlpha.300"
+              boxShadow="sm"
+            >
+              <Flex align="center" gap={2} mb={1}>
+                <Icon as={HiOutlineCalendar} color="blue.400" boxSize="16px" />
+                <Stat.Label color="fg" fontWeight="semibold">
+                  Next Episode
+                </Stat.Label>
+              </Flex>
+              <Stat.ValueText color="fg" fontSize="xl" fontWeight="bold">
+                {dayjs(nextEpisodeAirDate).format('MMM D, YYYY')}
+              </Stat.ValueText>
+              <Stat.HelpText color="blue.400" fontWeight="semibold">
+                {dayjs(nextEpisodeAirDate).fromNow()}
+              </Stat.HelpText>
+            </Stat.Root>
+          )}
           {lastEpisodeAirDate && (
             <Stat.Root
               p={4}
@@ -206,30 +229,16 @@ export const ShowDetails = () => {
               <Stat.Label color="fg.muted" fontWeight="medium">
                 Last Aired
               </Stat.Label>
-              <Stat.ValueText fontSize="xl" fontWeight="bold">
+              <Stat.ValueText
+                color="fg.muted"
+                fontSize="xl"
+                fontWeight="bold"
+                opacity={0.8}
+              >
                 {dayjs(lastEpisodeAirDate).format('MMM D, YYYY')}
               </Stat.ValueText>
-              <Stat.HelpText>
+              <Stat.HelpText color="fg.muted">
                 {dayjs(lastEpisodeAirDate).fromNow()}
-              </Stat.HelpText>
-            </Stat.Root>
-          )}
-          {nextEpisodeAirDate && (
-            <Stat.Root
-              p={4}
-              bg="whiteAlpha.50"
-              borderRadius="xl"
-              borderWidth="1px"
-              borderColor="whiteAlpha.100"
-            >
-              <Stat.Label color="fg" fontWeight="medium">
-                Next Episode
-              </Stat.Label>
-              <Stat.ValueText color="fg" fontSize="xl" fontWeight="bold">
-                {dayjs(nextEpisodeAirDate).format('MMM D, YYYY')}
-              </Stat.ValueText>
-              <Stat.HelpText color="fg">
-                {dayjs(nextEpisodeAirDate).fromNow()}
               </Stat.HelpText>
             </Stat.Root>
           )}
