dc05e7ff91bcb17ceefaa5e2e59879a34d91874a
trybick <t.rybicki@gmail.com>
2026-02-07 07:07:35 -0500

    Base SW

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -2,6 +2,7 @@ import path from 'node:path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   server: { open: true, port: 4000 },
@@ -9,5 +10,45 @@ export default defineConfig({
   resolve: {
     alias: { '~': path.resolve(__dirname, 'src') },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: false,
+      workbox: {
+        globPatterns: [],
+        navigateFallback: null,
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/api\.themoviedb\.org\/3\/.*/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'tmdb-api-cache',
+              expiration: {
+                maxEntries: 400,
+                maxAgeSeconds: 60 * 60 * 5,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/image\.tmdb\.org\/.*/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'tmdb-images-cache',
+              expiration: {
+                maxEntries: 500,
+                maxAgeSeconds: 60 * 60 * 24 * 30,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
 });
