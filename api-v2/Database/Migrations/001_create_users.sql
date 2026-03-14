1da1d205a6a58313b3248206104a93f63b9821b4
trybick <t.rybicki@gmail.com>
2026-03-14 15:09:54 -0400

    Start migrations

--- api-v2/Database/Migrations/001_create_users.sql
+++ api-v2/Database/Migrations/001_create_users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email TEXT NOT NULL UNIQUE
-    reated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 INSERT INTO users (id, email)
--- api-v2/Program.fs
+++ api-v2/Program.fs
@@ -1,9 +1,12 @@
+open DotNetEnv
 open Microsoft.AspNetCore.Builder
 open Giraffe
 open Giraffe.EndpointRouting
 
 [<EntryPoint>]
 let main args =
+    Env.Load() |> ignore
+
     let builder = WebApplication.CreateBuilder(args)
     builder.Services.AddGiraffe() |> ignore
     let app = builder.Build()
