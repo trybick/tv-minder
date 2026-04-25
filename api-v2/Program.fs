1af15825385ab67afc5ae6ce5626e287718689cf
trybick <t.rybicki@gmail.com>
2026-04-25 14:47:10 -0400

    Exception

--- api-v2/Program.fs
+++ api-v2/Program.fs
@@ -1,9 +1,14 @@
 open DotNetEnv
 open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.Logging
 open Giraffe
 open Giraffe.EndpointRouting
 
 let DEFAULT_PORT = "4600"
+
+let errorHandler (ex: exn) (logger: ILogger) : HttpHandler =
+    logger.LogError(ex, "Unhandled exception")
+    clearResponse >=> AppError.toHttp (AppError.Infrastructure (AppError.Unexpected ex.Message))
 
 let startApi (args: string array) =
     Database.runMigrations ()
@@ -16,6 +21,7 @@ let startApi (args: string array) =
     let app = builder.Build()
     app.Urls.Add $"http://0.0.0.0:{port}"
 
+    app.UseGiraffeErrorHandler(errorHandler) |> ignore
     app.UseRouting() |> ignore
     app.UseEndpoints(fun e -> e.MapGiraffeEndpoints Router.webApp) |> ignore
 
